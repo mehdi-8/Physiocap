2091f2241bcaec2859236ada15a65973ab9dcaef
mehdi-8 <50959187+mehdi-8@users.noreply.github.com>
2019-05-28 10:54:16 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+"""
+.. module:: test
+   :synopsis: premier module
+
+.. moduleauthor:: Micropole
+
+
+"""
+
+__docformat__ = 'restructuredtext'
+
+from docutils import utils
+from docutils.utils.error_reporting import ErrorOutput
+
+def print_string(str):
+    """
+        Fonction qui retourne une chaine.
+
+        Parameters:
+
+        - `str`: la chaine à imprimer.
+    """
+    print(str)
+
+def hello_world():
+    """
+        Fonction qui imprime un truc.
+
+        utilise la fonction `print_string(str)` pour la sortie écran
+    """
+    print_string('yalla!!')
---
+++ test2.py
@@ -0,0 +1,29 @@
+"""
+.. module:: test2
+   :synopsis: deuxieme module
+
+.. moduleauthor:: Micropole
+
+"""
+__docformat__ = 'restructuredtext'
+
+from docutils import utils
+from docutils.utils.error_reporting import ErrorOutput
+
+def print_string(str):
+    """
+    Fonction qui retourne une chaine.
+
+    Parameters:
+
+    - `str`: la chaine à imprimer.
+    """
+    print(str)
+
+def hello_world():
+    """
+    Fonction qui imprime un truc.
+
+    utilise la fonction `print_string(str)` pour la sortie écran
+    """
+    print_string('yalla!!')
